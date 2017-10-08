fdc186db97fffd2e3affb419ef376e82644c17d3
ZvezdocheTag <vyastrem@gmail.com>
2017-10-08 21:47:35 +0300

    basic edit logic

--- src/containers/InvoiceEdit/FormAdd.js
+++ src/containers/InvoiceEdit/FormAdd.js
@@ -3,18 +3,54 @@ import { Form, Control, actions } from 'react-redux-form';
 import { Button } from 'react-bootstrap'
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
+import TableCustom from '../../components/Table'
 
-var options = [
-  { value: 'one', label: 'One' },
-  { value: 'two', label: 'Two' }
-];
-function logChange(val) {
-  console.log("Selected: " + JSON.stringify(val));
+const InvoiceItem = ({data, keyId, event}) => (
+  <tr>
+      {/* <td>{data.name}</td>
+      <td>{data.price}</td> */}
+      <td>{data.name}</td>
+      <td>{data.price}</td>
+      <td>
+        <input type="text"/>
+      </td>
+  </tr>
+)
+
+function prepareDataToSelect(data) {
+  return data.map(item => 
+    ({ value: item.name, label: item.name, ...item})
+  )
 }
+
 class InvoiceFormAdd extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      customerVal: '',
+      productVal: ''
+    }
+  }
+
+  logChangeCustomer = (val) => {
+    let { dispatch } = this.props;
+    dispatch(actions.change("invoiceForm.add.customer", val))
+    this.setState({
+      customerVal: val
+    })
+  }
+
+  logChangeProduct = (val) => {
+    console.log(val)
+    this.setState({
+      productVal: val
+    })
+  }
+
   handleSubmit(values) {
-    // let { handlerCreateInvoice, close, dispatch } = this.props;
+    let { dispatch } = this.props;
     console.log(values)
+ 
     // handlerCreateInvoice(values.add)
     // .then(res => {
     //   close()
@@ -22,7 +58,43 @@ class InvoiceFormAdd extends React.Component {
     // })
   }
 
+  addProduct = (e) => {
+    // console.log(this, e.target)
+    
+    let { dispatch, formLiveProps } = this.props;
+    let { products } = formLiveProps;
+    
+    let condition = false;
+    for(
+      let i = 0;
+       i < products.length;
+       i+=1) {
+         if(products[i].id === this.state.productVal.id) {
+          condition = true;
+          break;
+         }
+        
+    }
+
+    if(!condition) {
+      dispatch(actions.push("invoiceForm.add.products", this.state.productVal))   
+    } else {
+      condition = false;
+    }
+
+
+  }
   render() {
+    let { customers, products, formLiveProps } = this.props;
+    let { customerVal, productVal }  = this.state;
+    let productList = formLiveProps.products
+    let  { discount } = formLiveProps;
+
+    let sum = productList.reduce((previousValue, item, index, arr) => {
+      return previousValue + item.price
+    }, 0)
+    let total = sum - (discount/100 * sum);
+
     return (
         <Form 
         model="invoiceForm" 
@@ -35,9 +107,10 @@ class InvoiceFormAdd extends React.Component {
           <label>Customer:</label>
           <Select
             name="form-field-name"
-            value="one"
-            options={options}
-            onChange={logChange}
+            simpleValue
+            value={customerVal}
+            options={prepareDataToSelect(customers)}
+            onChange={this.logChangeCustomer}
           />
         </div>
         <div className="field">
@@ -45,14 +118,25 @@ class InvoiceFormAdd extends React.Component {
           <div className="field-row">
           <Select
             name="form-field-name"
-            value="one"
-            options={options}
-            onChange={logChange}
+            value={productVal}
+            options={prepareDataToSelect(products)}
+            onChange={this.logChangeProduct}
           />
-          <Button bsStyle="primary">Add</Button>
+          <Button 
+          onClick={this.addProduct}
+          bsStyle="primary">Add</Button>
           </div>
         </div>
-
+        <TableCustom 
+                thead={[ "Name", "Price","Qty",]}
+                tbodyData={formLiveProps.products}
+                Tbitem={InvoiceItem}
+                update={this.update}
+                deleted={this.deleted}
+            />
+        <h2>
+          {total}
+        </h2>
         <div className="field-caption">
         <Button bsStyle="primary" type="submit">
           Submit
--- src/containers/InvoiceEdit/index.js
+++ src/containers/InvoiceEdit/index.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import { fetchInvoices } from './logic/action/fetchall'
+import { fetchCustomers } from '../Customers/logic/action/fetchall'
+import { fetchProducts } from '../Products/logic/action/fetchall'
+import { createInvoice } from './logic/action/create'
+import { deleteInvoice } from './logic/action/delete'
+import { changeInvoice } from './logic/action/change'
 import InvoiceFormAdd from './FormAdd'
+
+
 class InvoiceEdit extends Component {
+    componentWillMount() {
+        let { fetchProducts, fetchCustomers } = this.props;
+        fetchProducts()
+        fetchCustomers()
+    }
     render() {
+        let { customer, product, dispatch, invoiceForm } = this.props;
+        const { 
+            customers,
+            error,
+            loading
+         } = customer.all;
+        const { 
+            products,
+         } = product.all;
+
+
+        let condition = !customers.length && loading;
+         
         return (
             <div>
                 INVOICE EDIT
-                <InvoiceFormAdd />
+                {
+                    condition ?
+                        <div>load</div> :
+                        <InvoiceFormAdd 
+                        dispatch={dispatch}
+                        customers={customers}
+                        products={products}
+                        formLiveProps={invoiceForm.add}
+                        />
+                }
             </div>
         );
     }
@@ -16,4 +52,17 @@ InvoiceEdit.propTypes = {
 
 };
 
-export default InvoiceEdit;
+const mapStateToProps = (store) => ({...store})
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dispatch,
+        fetchInvoices: () => dispatch(fetchInvoices()),
+        fetchProducts: () => dispatch(fetchProducts()),
+        fetchCustomers: () => dispatch(fetchCustomers()),
+        createInvoice: (data) => dispatch(createInvoice(data)),
+        deleteInvoice: (id) => dispatch(deleteInvoice(id)),
+        changeInvoice: (data, id) => dispatch(changeInvoice(data, id)),   
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(InvoiceEdit);
